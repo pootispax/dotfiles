228fad09d3015f8b0adb9eaf345e5b6fec45d99e
Pax <olydaun@hotmail.fr>
2021-03-06 14:29:01 +0100

    oui

--- user/.zsh/aliases.zsh
+++ user/.zsh/aliases.zsh
@@ -2,3 +2,4 @@
 alias btop="sudo bpytop"
 alias luacomp="latexmk -lualatex -f --shell-escape"
 alias sp="subl command "close_project"; sleep 0.2; subl --project"
+alias gmx="/usr/local/gromacs/bin/gmx"
